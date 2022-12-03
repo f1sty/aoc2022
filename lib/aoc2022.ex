a8a3ac8dd6fe1636d4a77b6a0b5e4bae7cbb36b8
Yurii Skrynnykov <f1st@pm.me>
2022-12-03 15:52:14 +0200

    Add `read_puzzle_input/1` common function.

--- lib/aoc2022.ex
+++ lib/aoc2022.ex
@@ -2,4 +2,13 @@ defmodule Aoc2022 do
   @moduledoc """
   Module for common used functions.
   """
+
+  @puzzles_subdir "priv/puzzle_inputs"
+
+  def read_puzzle_input(filename) do
+    @puzzles_subdir
+    |> Path.absname()
+    |> Path.join(filename)
+    |> File.read!()
+  end
 end
