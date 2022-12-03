6ea13cd6654229a2cee1b6bf16334a06b88b5477
Yurii Skrynnykov <f1st@pm.me>
2022-12-03 16:07:03 +0200

    Add raw day 1 part 2 solution.

--- lib/aoc2022/day1.ex
+++ lib/aoc2022/day1.ex
@@ -8,4 +8,16 @@ defmodule Aoc2022.Day1 do
     end)
     |> Enum.max()
   end
+
+  def part2() do
+    "day1"
+    |> Aoc2022.read_puzzle_input()
+    |> String.split("\n\n")
+    |> Enum.map(fn numbers ->
+      numbers |> String.split() |> Enum.map(&String.to_integer/1) |> Enum.sum()
+    end)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
 end
--- test/aoc2022_day1_test.exs
+++ test/aoc2022_day1_test.exs
@@ -6,4 +6,8 @@ defmodule Aoc2022.Day1Test do
   test "solving part 1" do
     assert Day1.part1() == 71023
   end
+
+  test "solving part 2" do
+    assert Day1.part2() == 206_289
+  end
 end
