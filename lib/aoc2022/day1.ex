a9bbf82b530ea8e354a175a058e9d3b2a88b58b1
Yurii Skrynnykov <f1st@pm.me>
2022-12-08 22:30:05 +0200

    Refactor day 1 solution

--- lib/aoc2022/day1.ex
+++ lib/aoc2022/day1.ex
@@ -2,22 +2,24 @@ defmodule Aoc2022.Day1 do
   def part1() do
     "day1"
     |> Aoc2022.read_puzzle_input()
-    |> String.split("\n\n")
-    |> Enum.map(fn numbers ->
-      numbers |> String.split() |> Enum.map(&String.to_integer/1) |> Enum.sum()
-    end)
+    |> calories_per_elf()
     |> Enum.max()
   end
 
   def part2() do
     "day1"
     |> Aoc2022.read_puzzle_input()
+    |> calories_per_elf()
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
+  defp calories_per_elf(input) do
+    input
     |> String.split("\n\n")
     |> Enum.map(fn numbers ->
       numbers |> String.split() |> Enum.map(&String.to_integer/1) |> Enum.sum()
     end)
-    |> Enum.sort(:desc)
-    |> Enum.take(3)
-    |> Enum.sum()
   end
 end
