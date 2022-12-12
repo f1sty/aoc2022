5bbc3cc7a976435de5ea27132bcdc19a4744c441
Yurii Skrynnykov <truef1s7@gmail.com>
2022-12-12 19:51:46 +0200

    Add day 9 sceleton

---
+++ lib/aoc2022/day9.ex
@@ -0,0 +1,7 @@
+defmodule Aoc2022.Day9 do
+  @moduledoc false
+
+  def part1() do
+    # TODO:  <12-12-22, yourname> #
+  end
+end
---
+++ test/aoc2022_day9_test.exs
@@ -0,0 +1,9 @@
+defmodule Aoc2022Day9Test do
+  use ExUnit.Case, async: true
+
+  test "solving part 1" do
+  end
+
+  test "solving part 2" do
+  end
+end
